6f2102479c2a2674ce308a62f78150e0241293ec
jpkashlak <jpkashlak@gmail.com>
2021-01-16 10:33:07 -0600

    attached OWM's api key to the script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
-var test = document.getElementById("#title")
+var weatherFetch = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=581f1e37f61d004d56d5c92779c9ed8e"
+    fetch(weatherFetch).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+
+
+var test = document.querySelector("#testButton");
+
 var testFun = function() {
-    console.log("You did it!")
-}
-addEventListener("click", testFun)
+    console.log("Did it")
+};
+
+test.addEventListener("click", testFun);
